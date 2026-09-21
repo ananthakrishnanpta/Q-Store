cb11d9af419cef222f971188fd335659d2ef615b
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2026-09-21 11:10:05 +0530

    added router

--- frontend/src/app/router.jsx
+++ frontend/src/app/router.jsx
@@ -2,4 +2,21 @@ import HomePage from "../pages/HomePage/HomePage";
 import ContactPage from "../pages/ContactPage/ContactPage";
 import AboutPage from "../pages/AboutPage/AboutPage";
 
+import NotFound from "../pages/NotFound/NotFound";
 
+import PageLayout from "../components/layout/PageLayout/PageLayout";
+
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <PageLayout />,
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: "contact", element: <ContactPage /> },
+            { path: "about", element: <AboutPage /> },
+        ]
+    },
+    { path: "*", element: <NotFound /> }
+]);
--- frontend/src/components/layout/PageLayout/PageLayout.jsx
+++ frontend/src/components/layout/PageLayout/PageLayout.jsx
@@ -15,3 +15,6 @@ const PageLayout = () => {
         </>
     )
 }
+
+export default PageLayout;
+
